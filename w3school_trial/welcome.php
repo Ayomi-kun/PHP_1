6f9bd32c1bd0ae71d2bc116d87bbb9875c612827
Ayomiga <oluwayemi22ayo>
2017-07-12 15:45:44 +0100

    add

--- w3school_trial/welcome.php
+++ w3school_trial/welcome.php
@@ -1,8 +1,8 @@
 <html>
     <body>
-
+        <?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?><br>
         Welcome <?php echo $_POST["name"]; ?><br>
         Your email address is: <?php echo $_POST["email"]; ?>
 
     </body>
-</html>git
+</html>
