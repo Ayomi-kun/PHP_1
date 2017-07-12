120ca2e564a1486a63d81699208e05023308d111
Ayomiga <oluwayemi22ayo>
2017-07-12 15:31:52 +0100

    process

--- loginpage/login.php
+++ loginpage/login.php
@@ -3,8 +3,28 @@
 <!DOCTYPE>
 <html>
     <head>
-        <title> </title>
+        <title> Login page </title>
+        <link rel="stylsheet" type="text/css" href="style.css">
     </head>
     <body>
+        <div id="frm">
+            <form action="process.php" method="POST">
+
+                <p>
+                    <label>USERNAME:</label>
+                    <input type="text" id="user" name="user" />
+                </p>
+                 <p>
+                    <label>PASSWORD:</label>
+                    <input type="password" id="pass" name="pass" />
+                </p>
+                 <p>
+                    
+                    <input type="submit" id="btn" value="login" />
+                    
+                </p>
+
+            </form>
+        </div>
     </body>
 </html>
