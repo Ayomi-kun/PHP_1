4b9dd76cfe9bef2bcecb3f1e8c75454b198fc116
Ayomiga <oluwayemi22ayo>
2017-08-11 13:40:48 +0100

    update

--- loginpage/login.php
+++ loginpage/login.php
@@ -1,1 +1,3 @@
 <!--in this file we will be linking up to a php file and usuing database to validate and create a login profilegit-->
+<!--this file will also contain the major html file form and links-->
+<!DOCTYPE>
