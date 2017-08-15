ef26be48134b95438b0c9fb0c7b791bc85953f0c
Ayomiga <oluwayemi22ayo>
2017-08-15 11:32:57 +0100

    first commit successfull_patch

--- w3school_trial/welcome.php
+++ w3school_trial/welcome.php
@@ -1,4 +1,8 @@
 <html>
-    <head>
-        <title>form </
-</html>
+    <body>
+
+        Welcome <?php echo $_POST["name"]; ?><br>
+        Your email address is: <?php echo $_POST["email"]; ?>
+
+    </body>
+</html>git
