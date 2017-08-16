adb0ac5ef76b4ae3f4544adde2447b99abd6be15
Ayomiga <oluwayemi22ayo>
2017-08-16 10:13:43 +0100

    DIR

--- w3school_trial/welcome.php
+++ w3school_trial/welcome.php
@@ -1,7 +1,9 @@
 <html>
     <body>
+        <!--this below will show the directory of the opened file!!-->
         <!--<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>-->
-        <!--this above will show the directory of the opened file!!-->
+
+
         Welcome <?php echo $_POST["name"]; ?><br>
         Your email address is: <?php echo $_POST["email"]; ?>
 
