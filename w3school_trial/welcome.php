94d5531fc5c4757cf01228f88764f9f22f585193
Ayomiga <oluwayemiayo@yahoo.com>
2017-08-21 09:12:26 +0100

    assigning POST from html to variables for multiple recall

--- w3school_trial/welcome.php
+++ w3school_trial/welcome.php
@@ -2,10 +2,16 @@
     <body>
         <!--this below will show the directory of the opened file!!-->
         <!--<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>-->
+        <?php
+            $first_name = $_POST["first_name"];
+            $last_name = $_POST["last_name"];
+            $middle_name = $_POST["middle_name"];
+            $email = $_POST["email"];
 
+        ?>
 
-        Welcome <?php echo $_POST["name"]; ?><br>
-        Your email address is: <?php echo $_POST["email"]; ?>
+        Welcome <?php echo $first_name; ?><br>
+        Your email address is: <?php echo $email; ?>
 
     </body>
 </html>
