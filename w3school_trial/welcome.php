bb0216b3ebabd276e49702b88261df2bfa25a77c
Ayomiga <oluwayemiayo@yahoo.com>
2017-08-29 20:01:47 +0100

    securing data for php

--- w3school_trial/welcome.php
+++ w3school_trial/welcome.php
@@ -3,10 +3,21 @@
         <!--this below will show the directory of the opened file!!-->
         <!--<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>-->
         <?php
-            $first_name = $_POST["first_name"];
-            $last_name = $_POST["last_name"];
-            $middle_name = $_POST["middle_name"];
-            $email = $_POST["email"];
+        //define variables and set to empty values
+        $first_name = $last_name = $middle_name = $email = "";
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+                $first_name = test_input($_POST["first_name"]);
+                $last_name = test_input($_POST["last_name"]);
+                $middle_name = test_input($_POST["middle_name"]);
+                $email = test_input($_POST["email"]);
+        }
+
+        function test_input($data){
+            $data = trim($data);
+            $data = stripcslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
 
         ?>
 
