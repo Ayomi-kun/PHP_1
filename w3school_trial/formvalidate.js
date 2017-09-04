f7e209a12d71a7dad9341bc121b6483705a140a3
ayomiga <oluwayemiayo@yahoo.com>
2017-09-04 21:56:44 +0100

    finally validated form

--- w3school_trial/formvalidate.js
+++
@@ -1,7 +0,0 @@
-function validate_form(){
-    var x = document.forms(["myform"]["last_name"]).value;
-    if(x == null || x == ""){
-        alert("last_name" + "must be filled out");
-        return false;
-    }
-}
--- w3school_trial/welcome.php
+++ w3school_trial/welcome.php
@@ -5,13 +5,13 @@
         <?php
         // api's will also be intigrated into the code - eventually.
         //define variables and set to empty values
-        $first_name = $last_name = $middle_name = $email =  $gender ="";
+        $first_name = $last_name = $username = $email =  $gender ="";
         // creating variables to handle error.
-        $first_name_error = $last_name_error = $middle_name_error = $email_error = $gender_error = "";
+        $first_name_error = $last_name_error = $username_error = $email_error = $gender_error = "";
 
         $first_name = $_POST["first_name"];
         $last_name = $_POST["last_name"];
-        $middle_name = $_POST["middle_name"];
+        $username = $_POST["username"];
         $email = $_POST["email"];
         $gender = $_POST["gender"];
 
@@ -21,38 +21,6 @@
             $data = htmlspecialchars($data);
             return $data;
         }
-
-
-
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            if(empty($first_name)){
-                $first_name_error = "Name is required";
-            }else{
-                $first_name = test_input($first_name);
-            }
-            if(empty($last_name)){
-                $last_name_error = "last name is required";
-            }else{
-                $last_name = test_input($last_name);
-            }
-            if(empty($middle_name)){
-                $middle_name = "";
-            }else{
-                $middle_name = test_input($middle_name);
-            }
-            if(empty($email)){
-                $email_error = "Email required";
-            }else{
-                $email = test_input($email);
-            }
-            if(empty($gender)){
-                $gender_error = "please select the gender of your type";
-            }else{
-                $gender = test_input($gender);
-            }
-        }
-
-      
         ?>
 
         Welcome <?php echo $first_name; ?><br>
