9ef7694d206f78dcb9ba4c6a0b744314de4a7ed8
ayomiga <oluwayemiayo@yahoo.com>
2017-09-14 13:17:00 +0100

    thursday

--- php_crud/database.php
+++ php_crud/database.php
@@ -1,36 +1,37 @@
 <?php
 class Database
 {
-    private static $dbName = 'php_crud' ;
-    private static $dbHost = 'localhost:8080';
+    private static $dbName = 'crud_tutorial' ;
+    private static $dbHost = 'localhost' ;
     private static $dbUsername = 'root';
-    private static $dbUserpassword ='oluwayemi22ayo';
-
-    private static $cont = null;
-
-    public function __construct(){
+    private static $dbUserPassword = '';
+     
+    private static $cont  = null;
+     
+    public function __construct() {
         die('Init function is not allowed');
     }
-
+     
     public static function connect()
     {
-        //one connection through the whole application
-        if (null == self::$cont)
+       // One connection through whole application
+       if ( null == self::$cont )
+       {     
+        try
         {
-            try{
-                self::$cont = new PDO("mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserpassword)
-            }
-            catch(PDOException $e){
-                die($e->getMessage());
-            }
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword); 
         }
-        return self::$cont;
+        catch(PDOException $e)
+        {
+          die($e->getMessage()); 
+        }
+       }
+       return self::$cont;
     }
-
+     
     public static function disconnect()
-        {
-            self::$cont = null;
-        }
-
+    {
+        self::$cont = null;
+    }
 }
 ?>
--- php_mysql/classes_methods.php
+++ php_mysql/classes_methods.php
@@ -3,7 +3,9 @@
 
     class Person{//creatting a class - Person
         function say_hello(){//creating a method 
-            echo "Hello from inside a class.<br />";
+            echo "Hello from inside the class ".get_class($this)."<br />";
+            // get_ class($this)this just prints out the name of the class.
+            // $this is the way we can refer to instances and call these instances inside the class...only inside the class 
         }
     }
 
@@ -17,4 +19,37 @@
     else{
         echo "Method does not exist.<br />";
     }
+    echo "<hr />";
+    $person = new Person();
+
+
+    $person->say_hello();
+    echo "<hr />";
+    
+    class Human{
+        var $first_name;
+        var $last_name;
+
+        var $arm_count = 2;
+        //creating elements of this class
+        var $leg_count = 2; 
+
+    }
+    $human = new Human();
+    //creating an instance of the class
+    //echo $human->arm_count."<br />";
+    //  getting an element(variable in the class) from the class from the class through the object
+    //no need for adding $ sign when refrencing a variable from the class 
+    $human->arm_count=4;
+    echo $human->arm_count."<br />";
+    // this changes the default arm count of the object human instance to 4
+    $human->first_name = "Idris";
+    echo $human->first_name."<br />";
+
+    $human2 = new Human();
+    $human2->first_name = "Ayo";
+    echo $human2->first_name."<br />";
+    echo $human2 -> arm_count."<br />";
+    
+
 ?>
